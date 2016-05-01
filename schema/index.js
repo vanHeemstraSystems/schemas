92b2fdd3d4e5cca19e08edd11652cfa06a97b2ea
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-01 13:01:09 +0100

    Modified

--- schema/index.js
+++ schema/index.js
@@ -7,11 +7,46 @@ var self = this; // set the context locally, for access protection
  * @return {Schema}
  */
 function Schema() {
-  console.log('schemas schema - Schema called');   
+  console.log('schemas schema - Schema called'); 
+  // add key value pairs here
+  // self's are not directly publicly accessible, only through their public method(s)
+  // use self's here for protection from direct access
+  self._error = {};  // will be set, before passing on to mapping  
+  self._type = {}; // will be set, before passing on to mapping
+  self._utility = {};   // will be set, before passing on to mapping  
+}
+
+Schema.prototype.error = function() {
+  return self._error;
+}
+
+Schema.prototype.seterror = function(fnOrValue) {
+  self._error = fnOrValue;
+}
+
+Schema.prototype.type = function() {
+  return self._type;
+}
+
+Schema.prototype.settype = function(fnOrValue) {
+  self._type = fnOrValue;
+}
+
+Schema.prototype.utility = function() {
+  return self._utility;
+}
+
+Schema.prototype.setutility = function(fnOrValue) {
+  self._utility = fnOrValue;
 }
 
 Schema.prototype.rethinkdb = function() {
-  return new SchemaRethinkDB();
+  this._schemarethinkDB = new SchemaRethinkDB();
+  this._schemaRethinkDB.seterror(self.error());
+  this._schemaRethinkDB.settype(self.type());
+  this._schemaRethinkDB.setutility(self.utility());  
+  //ORIGINAL return new SchemaRethinkDB();
+  return this._schemaRethinkDB;
 }
 
 module.exports = Schema;
